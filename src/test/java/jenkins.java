49514bd91d3484f5b9b897c0ac240e997e6c0a26
Tolgonai23 <bakton1005@gmail.com>
2019-07-15 21:00:13 -0500

    My first Jenkins

---
+++ src/test/java/jenkins.java
@@ -0,0 +1,13 @@
+public class jenkins {
+
+    public static void main(String[] args) {
+        for (int j = 0; j <= 6; j++) {
+            if (j == 4) {
+                continue;
+            }
+
+            System.out.print(j + " ");
+        }
+
+    }
+}
